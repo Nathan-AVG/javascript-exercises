c36b2f0e6d4953c07f26036fbe00f7f2da1b6646
Nathan <rensly2208@gmail.com>
2022-08-30 16:48:14 +0800

    one last exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newtemp = 0;
+  newtemp = (temp - 32) * 5 / 9;
+  newtemp = Math.round(newtemp * 10) / 10
+  console.log(newtemp);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let newtemp = 0;
+  newtemp = temp * 9/5 + 32
+  newtemp = Math.round(newtemp * 10) / 10
+  console.log(newtemp);
 };
+
+ftoc(80);
 
 // Do not edit below this line
 module.exports = {
